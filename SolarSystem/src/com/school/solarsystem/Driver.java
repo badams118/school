09a8368e9bfab10ce1c33cf5282ed2276eefe818
Brady Adams <brady@netzero.net>
2017-10-02 17:37:05 -0600

    Added supress warnings

--- SolarSystem/src/com/school/solarsystem/Driver.java
+++ SolarSystem/src/com/school/solarsystem/Driver.java
@@ -2,6 +2,7 @@ package com.school.solarsystem;
 
 public class Driver {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		SolarSystem solarSystem = new SolarSystem();
 	}
